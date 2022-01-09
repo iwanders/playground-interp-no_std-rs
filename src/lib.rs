41a752d399029db3f338f17175148758231efa6d
Ivor Wanders <ivor@iwanders.net>
2022-01-09 13:27:44 -0500

    Fix memcmp, split out support.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 // here we go :o
 #![feature(asm)]
 mod syscall;
+mod support;
 pub use crate::syscall::{exit, write};
 
 pub fn print(input: &str) {
@@ -49,41 +50,6 @@ impl fmt::Write for StackString {
     }
     // fn write_char(&mut self, c: char) -> Result { ... }
     // fn write_fmt(&mut self, args: Arguments<'_>) -> Result { ... }
-}
-// Well, format just assumes that memset and memcpy exist.
-// We can definitely provide those to make the linker happy, they don't require allocations.
-#[no_mangle]
-pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, size: usize) -> *mut u8 {
-    let mut i = 0;
-    // for i in 0..size
-    while i < size {
-        *dest.offset(i as isize) = *src.offset(i as isize);
-        i += 1;
-    }
-    dest
-}
-#[no_mangle]
-pub unsafe extern "C" fn memset(ptr: *mut u8, fill: i32, size: usize) -> *mut u8 {
-    let mut i = 0;
-    while i < size {
-        (*ptr.offset(i as isize)) = fill as u8;
-        i += 1;
-    }
-    ptr
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, size: usize) -> i32 {
-    let mut i = 0;
-    while i < size {
-
-        if *s1.offset(i as isize) != *s2.offset(i as isize)
-        {
-            return 1; // something about the sign...
-        }
-        i += 1;
-    }
-    0
 }
 
 // use core::fmt;
