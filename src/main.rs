de1fb47f140fb29b24deece2ae68469dc21beb1f
Ivor Wanders <ivor@iwanders.net>
2022-01-09 17:06:41 -0500

    Minimal non working example for segfault in grisu.

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,27 @@
 #![no_std]
 #![no_main]
 
-// here we go :o
-// #![feature(asm)]
+mod support;
 
-// use syscall_test;
-extern crate syscall_test;
-use syscall_test::{exit, print};
+struct WritableThing{}
+use core::fmt::Error;
+impl fmt::Write for WritableThing {
+    fn write_str(&mut self, s: &str) -> Result<(), Error> {
+        Ok(())
+    }
+}
 
-use syscall_test::{fmt, fmt::Write, print_sstr, println, StackString};
+use core::fmt;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // write();
-    // print("hello");
-    // printauxv();
-    // printb("b");
-    // print("hello");
-    // println!("{} haha", 1);
-
-    for i in 0..10 {
-        // println!("Lorem {} ipsum {:?} dolor {} ", 5, Some(i), "foo");
-        println!("Lorem {} ipsum {:?} dolor {:?} ", 5, Some(i), Some(3.3));
-        // println!("Lorem {} ipsum {:?} dolor {} ", 5, Some(3.3), "foo");
-    }
-
-    // stackallocate();
-
-    // exit(0);
-
-    for _i in 0..1 {
-        print(".");
-    }
-    // lets do some stack exhaustion and see where it fails...
-    // recurser(0);
-    // x0000555555554505 in test::recurser (z=262007) at src/main.rs:218
-    // 1048028 bytes... that's not too bad... sounds like my stack string should also work??
-
-    // Unless that... grows from the other side or something?
-
-    print("z");
-    print("x\n");
-
-    exit(0);
+    let mut v: WritableThing = WritableThing{};
+    fmt::write(&mut v, format_args!("{}", 3.3)).expect("Error occurred while trying to write in String");
     loop {}
 }
 
 use core::panic::PanicInfo;
 #[panic_handler]
 pub fn panic(_info: &PanicInfo) -> ! {
-    print("\nPanic!");
-    exit(99);
     loop {}
 }
