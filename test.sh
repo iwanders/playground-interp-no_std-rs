2489abc485b980d623a53c3a9b4a1e437add2118
Ivor Wanders <ivor@iwanders.net>
2022-01-25 19:05:16 -0500

    Testing for issue 361.

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -v
+
+
+cargo +nightly --version
+
+echo ${__CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS}
+echo ${CARGO_UNSTABLE_TARGET_APPLIES_TO_HOST}
+echo ${CARGO_TARGET_APPLIES_TO_HOST}
+
+rm -rf target
+cargo +nightly build --target x86_64-unknown-linux-gnu
+objdump -T ./target/debug/build/compiler_builtins-1d5a76e82d9e8bdd/build-script-build  | grep main
+cargo b -Zunstable-options --build-plan  --target x86_64-unknown-linux-gnu > /tmp/build_plan_with_target.json
+
+
+rm -rf target
+export __CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS="nightly"
+export CARGO_UNSTABLE_TARGET_APPLIES_TO_HOST="true"
+export CARGO_TARGET_APPLIES_TO_HOST="false"
+
+echo ${__CARGO_TEST_CHANNEL_OVERRIDE_DO_NOT_USE_THIS}
+echo ${CARGO_UNSTABLE_TARGET_APPLIES_TO_HOST}
+echo ${CARGO_TARGET_APPLIES_TO_HOST}
+cargo --version
+echo "--"
+cargo build
+echo "--"
+objdump -T ./target/debug/build/compiler_builtins-1d5a76e82d9e8bdd/build-script-build  | grep main
+cargo b -Zunstable-options --build-plan > /tmp/build_plan.json
